48e2758e71b03933e1cd2cfc6b6f8180bfbe9ec9
Ayush Krishnappa <ayushkrishnappa@gmail.com>
2024-09-13 14:31:47 -0400

    Initial Commit

---
+++ StyleSwipe/StyleSwipeApp.swift
@@ -0,0 +1,20 @@
+//
+//  StyleSwipeApp.swift
+//  StyleSwipe
+//
+//  Created by Ayush Krishnappa on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleSwipeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
