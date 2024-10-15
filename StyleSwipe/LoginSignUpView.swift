1154e545e388691d5e27039397cf33be907c4f99
Tanisha <tanishar254@gmail.com>
2024-10-15 15:37:15 -0400

    created login/sign up page

---
+++ StyleSwipe/LoginSignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginSignUpView.swift
+//  StyleSwipe
+//
+//  Created by Tanisha Rahim on 10/15/24.
+//
+
+import SwiftUI
+
+struct LoginSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginSignUpView()
+}
