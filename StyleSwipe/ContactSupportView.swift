5cc68078665605843295fa64dabb048877237202
Tanisha <tanishar254@gmail.com>
2024-11-01 12:08:57 -0400

    Profile Page and Settings Page

---
+++ StyleSwipe/ContactSupportView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactSupportView.swift
+//  StyleSwipe
+//
+//  Created by Tanisha Rahim on 10/30/24.
+//
+
+import SwiftUI
+
+struct ContactSupportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactSupportView()
+}
---
+++ StyleSwipe/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  StyleSwipe
+//
+//  Created by Tanisha Rahim on 10/25/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
